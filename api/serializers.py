5ab1f02a3fe4cc91383fd6f102780c505bc14ce9
pujan <lukasz.pelc.81@gmail.com>
2021-08-02 11:29:05 +0200

    Image field for author, book, publisher

--- api/serializers.py
+++ api/serializers.py
@@ -5,13 +5,17 @@ from rest_framework import serializers
 # Serializers define the API representation.
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
     uri = serializers.SerializerMethodField('get_uri')
+    number_books = serializers.SerializerMethodField('get_number_books')
 
     class Meta:
         model = Author
-        fields = ['id', 'firstname', 'lastname', 'biography', 'uri']
+        fields = ['id', 'firstname', 'lastname', 'biography', 'uri', 'photo', 'number_books']
 
     def get_uri(self, obj):
         return f'/authors/{obj.slug}/'
+
+    def get_number_books(self, obj):
+        return obj.number_books()
 
 
 class PublisherSerializer(serializers.HyperlinkedModelSerializer):
